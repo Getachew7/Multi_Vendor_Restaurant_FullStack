97a3a1a5a4101f6a627d8fad620228a04fd887a5
getachewyazie <getchyaz7@gmail.com>
2022-08-25 02:10:40 -0700

    some changes

--- Multi_Vendor_Restaurant/settings.py
+++ Multi_Vendor_Restaurant/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Multi_Vendor_Restaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Multi_Vendor_Restaurant/views.py
+++ Multi_Vendor_Restaurant/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hello we are starting the project Its Lite Im PUMPED...</h1>")
+    return render(request, 'home.html')
