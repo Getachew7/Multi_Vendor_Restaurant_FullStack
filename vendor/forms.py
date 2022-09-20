5a28eb00e6777cfc050498c6975a5aeadb98c3a5
getachewyazie <getchyaz7@gmail.com>
2022-09-20 11:38:51 -0800

    added vendor profile custom validator

--- vendor/forms.py
+++ vendor/forms.py
@@ -1,9 +1,9 @@
-from dataclasses import field
-from socket import fromshare
+from account.validator import allow_only_images_validator
 from django import forms
 from .models import Vendor
 
 class VendorForm(forms.ModelForm):
+    vendor_licence = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_licence']
